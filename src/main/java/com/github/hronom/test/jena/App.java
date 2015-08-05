875c858110e22d8b5b39260267c07dbd26384196
Hronom <hronom@gmail.com>
2015-08-06 00:16:49 +0300

    Update test variants

--- src/main/java/com/github/hronom/test/jena/App.java
+++ src/main/java/com/github/hronom/test/jena/App.java
@@ -4,9 +4,16 @@ import com.hp.hpl.jena.graph.Graph;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.sparql.core.DatasetGraph;
+import com.hp.hpl.jena.sparql.core.DatasetImpl;
 import com.hp.hpl.jena.tdb.TDBFactory;
-import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+
+import java.util.Iterator;
 
 public class App {
     public static void main(String[] args) {
@@ -38,12 +45,17 @@ public class App {
             )
         );
 
+        testIterList(graph);
+        testTransactions(datasetGraph, graph);
+
+        RDFDataMgr.write(System.out, graph, RDFFormat.NTRIPLES);
+    }
+
+    public static void testIterList(Graph graph) {
         // Test
-        ExtendedIterator<Triple> iter = graph.find(
-            Node.ANY,
-            NodeFactory.createURI("creationYear"),
-            NodeFactory.createURI("2015")
-        );
+        Iterator<Triple> iter = graph.find(
+            Node.ANY, NodeFactory.createURI("creationYear"), NodeFactory.createURI("2015")
+        ).toList().iterator();
 
         while (iter.hasNext()) {
             Triple triple = iter.next();
@@ -51,10 +63,29 @@ public class App {
 
             // Exception here.
             graph.add(
-                new Triple(
-                    subject, NodeFactory.createURI("status"), NodeFactory.createURI("available")
-                )
+                new Triple(subject, NodeFactory.createURI("value"), NodeFactory.createLiteral("1"))
             );
         }
+    }
+
+    public static void testTransactions(DatasetGraph datasetGraph, Graph graph) {
+        // Test
+        Iterator<Triple> iter = graph.find(
+            Node.ANY, NodeFactory.createURI("creationYear"), NodeFactory.createURI("2015")
+        ).toList().iterator();
+
+        Dataset dataset = DatasetImpl.wrap(datasetGraph);
+        dataset.begin(ReadWrite.WRITE);
+        while (iter.hasNext()) {
+            Triple triple = iter.next();
+            Node subject = triple.getSubject();
+
+            // Exception here.
+            graph.add(
+                new Triple(subject, NodeFactory.createURI("value"), NodeFactory.createLiteral("2"))
+            );
+        }
+        dataset.commit();
+        dataset.end();
     }
 }
