b98c84e1a3115974bbbf37bb1d40dff16e0eadbd
Hronom <hronom@gmail.com>
2015-08-05 22:09:34 +0300

    Initial commit

---
+++ src/main/java/com/github/hronom/test/jena/App.java
@@ -0,0 +1,59 @@
+package com.github.hronom.test.jena;
+
+import com.hp.hpl.jena.graph.Graph;
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.sparql.core.DatasetGraph;
+import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+public class App {
+    public static void main(String[] args) {
+        DatasetGraph datasetGraph = TDBFactory.createDatasetGraph();
+        Graph graph = datasetGraph.getDefaultGraph();
+
+        // Fill graph.
+        graph.add(
+            new Triple(
+                NodeFactory.createURI("www.test.org/unit13"),
+                NodeFactory.createURI("type"),
+                NodeFactory.createURI("robot")
+            )
+        );
+
+        graph.add(
+            new Triple(
+                NodeFactory.createURI("www.test.org"),
+                NodeFactory.createURI("isCombat"),
+                NodeFactory.createLiteral("true")
+            )
+        );
+
+        graph.add(
+            new Triple(
+                NodeFactory.createURI("www.test.org"),
+                NodeFactory.createURI("creationYear"),
+                NodeFactory.createURI("2015")
+            )
+        );
+
+        // Test
+        ExtendedIterator<Triple> iter = graph.find(
+            Node.ANY,
+            NodeFactory.createURI("creationYear"),
+            Node.ANY
+        );
+
+        while (iter.hasNext()) {
+            Triple triple = iter.next();
+            Node subject = triple.getSubject();
+
+            graph.add(
+                new Triple(
+                    subject, NodeFactory.createURI("status"), NodeFactory.createURI("available")
+                )
+            );
+        }
+    }
+}
